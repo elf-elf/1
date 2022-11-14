a9ebac7bf8cd73671e1abb33ffffebe5a96eacd6
elf-elf <vasenkinalena@gmail.com>
2022-11-14 15:44:36 +0400

    1

---
+++ tests/src/test/java/guru/qa/test/TestForms.java
@@ -0,0 +1,10 @@
+package guru.qa.test;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestForms {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+}
